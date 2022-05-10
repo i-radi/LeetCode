17827b8a87a802c7cb2c5d66c8083ff987ba981d
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-05-11 00:19:47 +0200

    Time: 100 ms (76.30%), Space: 34.8 MB (84.97%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cs
+++ 216-combination-sum-iii/216-combination-sum-iii.cs
@@ -1,30 +1,24 @@
 public class Solution {
     public IList<IList<int>> CombinationSum3(int k, int n) {
         var ans = new List<IList<int>>();
-        
-        for(int i=0;i<(1<<9);i++)
+        DFS(k,n,1,0,new List<int>(), ans);
+        return ans;
+    }
+    
+    private void DFS(int k, int n, int index, int sum, List<int> list, List<IList<int>> ans)
+    {
+        if(list.Count == k)
         {
-            var list = new List<int>();
-            var sum = 0;
-            var countOf1 = 0;
-            for(var j=1;j<=9;j++)
-            {
-                if((i & (1 << (j-1))) > 0)
-                {
-                    sum+=j;
-                    countOf1++;
-                    list.Add(j);
-                }
-                
-                if(countOf1 > k) break;
-            }
-            
-            if(countOf1 == k && sum == n)
-            {
-                ans.Add(list);
-            }
+            if(sum == n)
+                ans.Add(new List<int>(list));
+            return;
         }
         
-        return ans;
+        for(int i=index;i<=9;i++)
+        {
+            list.Add(i);
+            DFS(k,n,i+1,sum+i,list,ans);
+            list.RemoveAt(list.Count-1);
+        }
     }
 }
