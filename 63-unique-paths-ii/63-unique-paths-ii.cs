f18615959da4abe196789f3dfa9694a28c4321c4
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-05-20 02:09:47 +0200

    Time: 106 ms (59.40%), Space: 37.1 MB (77.11%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cs
+++ 63-unique-paths-ii/63-unique-paths-ii.cs
@@ -1,22 +1,33 @@
 public class Solution {
-   public int UniquePathsWithObstacles(int[][] obstacleGrid) {
-       var m = obstacleGrid.Length+1;
-       var n = obstacleGrid[0].Length+1;
-
-       var DP = new int[m,n];
-      
-       for(int i=1;i<m;i++){
-           for(int j=1;j<n;j++){
-              if(obstacleGrid[i-1][j-1] == 1){
-                  DP[i,j] = 0;
-              }else if(i==1 && j==1){
-                  DP[i,j] = 1;
-              }else{
-                  DP[i,j] = DP[i-1,j] + DP[i,j-1];
-              }
-           }
-       }
-
-       return DP[m-1,n-1];               
-   }
+    public int UniquePathsWithObstacles(int[][] obstacleGrid) {
+        var dp = new int[obstacleGrid.Length, obstacleGrid[0].Length];
+        
+        for(int i=0;i< obstacleGrid.Length;i++){
+            if(obstacleGrid[i][0] == 1){
+                break;
+            }else{
+                dp[i,0] = 1;
+            }
+        }
+        
+        for(int j=0;j < obstacleGrid[0].Length; j++){
+            if(obstacleGrid[0][j] == 1){
+                break;
+            }else{
+                dp[0,j] = 1;
+            }
+        }
+        
+        for(int i=1;i<obstacleGrid.Length;i++){
+            for(int j=1;j<obstacleGrid[0].Length;j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i,j] = 0;
+                }else{
+                    dp[i,j] = dp[i-1,j] + dp[i,j-1];
+                }
+            }
+        }
+        
+        return dp[obstacleGrid.Length -1, obstacleGrid[0].Length - 1];
+    }
 }
