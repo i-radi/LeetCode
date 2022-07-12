6b4abb32c15b3a6f223dc045da8daef6ab0933f8
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-07-12 11:29:20 +0200

    Time: 97 ms (54.07%), Space: 35 MB (47.16%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cs
+++ 647-palindromic-substrings/647-palindromic-substrings.cs
@@ -1,29 +1,27 @@
 public class Solution {
-  
     public int CountSubstrings(string s) {
-        var n = s.Length;
-        var ans = 0;
-        var memo = new bool[n,n];
-        
-        for(int len = 1;len <= n;len++)
-        {
-            for(int l = 0; l+len-1 < n;l++)
-            {
-                var r = l + len-1;
-                if(IsPalindromic(s, l , r, memo))
-                {
-                    memo[l,r] = true;
-                    ans++;
-                }
-            }
+        var res = 0;
+        for (int i=0; i < s.Length; i++){
+            res += GetPalindromesAroundCenter(s, i);
         }
-        
-        return ans;
+        return res;
     }
     
-    private bool IsPalindromic(string s, int l , int r, bool[,] memo)
-    {
-        if(s[l] == s[r] && (r - l <=2 || memo[l+1,r-1])) return true;
-        return false;
+    private int GetPalindromesAroundCenter(string str, int center){
+        var count = 1;
+        count += GetPalindromeCount(str, center - 1,center + 1);
+        count += GetPalindromeCount(str, center,center + 1);
+        return count;
+    }
+    
+    private int GetPalindromeCount(string str, int start, int end){
+        var count = 0;
+        while(start >= 0 && end < str.Length){
+            if (str[start--] != str[end++]){
+                break;
+            }
+            count ++;
+        }
+        return count;
     }
 }
