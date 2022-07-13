29f3fd143b05f3a5e72ff8817f697906420ae864
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-07-13 12:01:40 +0200

    Time: 134 ms (50.52%), Space: 38.8 MB (88.24%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cs
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cs
@@ -1,20 +1,31 @@
 public class Solution {
     public string FrequencySort(string s)
     {
-        Dictionary<char, int> dict = new Dictionary<char, int>();
+        var freqMap = new Dictionary<char, int>();
         foreach(var c in s)
         {
-            if (!dict.ContainsKey(c)) dict.Add(c, 0);
-                dict[c] += 1;
+            if (!freqMap.ContainsKey(c)) freqMap.Add(c, 0);
+                freqMap[c] ++;
         }
 
-        var sortedList = dict.ToList().OrderByDescending(n => n.Value);
-        var sb = new StringBuilder();
-        foreach(var k in sortedList)
-        {
-            sb.Append(k.Key, k.Value);
+        var pq = new PriorityQueue<char, int>(new FreqComparer());
+        foreach(var key in freqMap.Keys){
+            pq.Enqueue(key, freqMap[key]);
         }
+        var res = new StringBuilder();
+        while(pq.Count > 0){
+            var currentChar = pq.Dequeue();
+            var freq = freqMap[currentChar];
+            for(var i = 0; i < freq; i++){
+                res.Append(currentChar);
+            }
+        }
+        return res.ToString();
+    }
+}
 
-        return sb.ToString();
+public class FreqComparer: IComparer<int>{
+    public int Compare(int a, int b){
+        return b - a;
     }
 }
