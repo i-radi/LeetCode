d930441c7f7cdf29780eb075466323fec671ae66
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-07-13 15:09:54 +0200

    Time: 213 ms (43.12%), Space: 43.6 MB (9.66%) - LeetHub

--- 46-permutations/46-permutations.cs
+++ 46-permutations/46-permutations.cs
@@ -1,22 +1,21 @@
 public class Solution {
     public IList<IList<int>> Permute(int[] nums) {
-        var res = new List<IList<int>>();
-        PermuteRec(new HashSet<int>(nums), new List<int>(), res);
-        return res;
+        return PermuteRec(new HashSet<int>(nums), new List<int>());
     }
     
-   private void PermuteRec(HashSet<int> nums, List<int> prefix, IList<IList<int>> res){
+   private List<IList<int>> PermuteRec(HashSet<int> nums, List<int> prefix){
+       var res = new List<IList<int>>();
        if (nums.Count == 0){
            res.Add(new List<int>(prefix));
-           return;
        }
        var remainingNums = new HashSet<int>(nums);
        foreach(var c in nums){
            remainingNums.Remove(c);
            prefix.Add(c);
-           PermuteRec(remainingNums, prefix, res);
+           res.AddRange(PermuteRec(remainingNums, prefix));
            remainingNums.Add(c);
            prefix.RemoveAt(prefix.Count - 1);
        }
+       return res;
    }
 }
