1a99e09646d56ff5285afe0f11194abac66c4737
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-07-15 13:24:17 +0200

    Time: 189 ms (5.11%), Space: 37.7 MB (83.36%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while(head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
