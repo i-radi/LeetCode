45fb10b6a23bc87854ccf81c8e0efdccdbed525f
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-07-18 11:49:05 +0200

    Time: 180 ms (23.90%), Space: 39.8 MB (58.88%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cs
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cs
@@ -11,20 +11,15 @@
  *     }
  * }
  */
-public class Solution
-{
-    public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
-    {
-        if (t1 == null && t2 == null) return null;
-        int value = 0;
-        if (t1 != null) value += t1.val;
-        if (t2 != null) value += t2.val;
-
-        TreeNode node = new TreeNode(value);
-        node.left = MergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
-        node.right = MergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
-
-        return node;
+public class Solution {
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return null;
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+        root1.val += root2.val;
+        root1.left = MergeTrees(root1.left, root2.left);
+        root1.right = MergeTrees(root1.right, root2.right);
+        
+        return root1;
     }
 }
-
