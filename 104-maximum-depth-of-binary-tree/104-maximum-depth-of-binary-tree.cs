27e3cebd3a7c30ced1f1f727c1859323f41b154e
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-07-21 11:52:02 +0200

    Time: 131 ms (48.59%), Space: 37.7 MB (83.43%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            else
+            {
+                return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+            }
+        }
+}
