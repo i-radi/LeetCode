b0d0ca3f8dcd6b8cba85f0e6523ed5f569ec78ed
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-05 13:22:38 +0200

    Time: 97 ms (81.38%), Space: 38.9 MB (14.68%) - LeetHub

---
+++ 86-partition-list/86-partition-list.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode Partition(ListNode head, int x) {
+        
+        ListNode before_head = new ListNode();
+        ListNode before = before_head;
+        ListNode after_head = new ListNode();
+        ListNode after = after_head;
+
+        while (head != null) {
+
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+        after.next = null;
+        before.next = after_head.next;
+
+        return before_head.next;
+    }
+}
