7c909c292d9cd9e59b59236865f4f24c00b627da
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-06 06:22:40 +0200

    Time: 154 ms (18.18%), Space: 37.6 MB (56.82%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int ans = 0;
+    public int DistributeCoins(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+    
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int L = dfs(node.left);
+        int R = dfs(node.right);
+        ans += Math.Abs(L) + Math.Abs(R);
+        return node.val + L + R - 1;
+    }
+}
