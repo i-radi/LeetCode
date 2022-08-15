ffdc2a8ed9aa7e9b87b23169ecf6784b5eb4e26c
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-15 05:48:33 +0200

    Time: 123 ms (81.35%), Space: 40 MB (60.12%) - LeetHub

---
+++ 136-single-number/136-single-number.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int SingleNumber (int[] nums) {
+        int ret = 0;
+
+        foreach (var i in nums) {
+            ret ^= i;
+        }
+
+        return ret;
+    }
+}
