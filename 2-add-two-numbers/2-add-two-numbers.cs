0f0ace583c31ff3c5cbad12fd4727988ce7ccc9e
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-19 08:25:43 +0200

    Time: 140 ms (52.44%), Space: 39.7 MB (23.44%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cs
+++ 2-add-two-numbers/2-add-two-numbers.cs
@@ -7,42 +7,40 @@
  * }
  */
 public class Solution {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-       var valAndrem=FindValue(l1.val, l2.val, false);
-        ListNode head=new ListNode(valAndrem.valu);
-        ListNode l3=head;
-        l1=l1.next; l2=l2.next;
-        while (l1!=null || l2!=null){
-            if (l1==null && l2!=null){
-                var valAndrem2=FindValue(0, l2.val, valAndrem.remainder);
-                l3.next=new ListNode(valAndrem2.valu);
-                l3=l3.next; l2=l2.next;
-                valAndrem=valAndrem2;
+public ListNode AddTwoNumbers(ListNode l1, ListNode l2){
+            if (l1 == null && l2 == null) return null;
+
+            ListNode preHead = new ListNode(-1);
+            ListNode current = preHead;
+            ListNode p1 = l1;
+            ListNode p2 = l2;
+            int carry = 0;
+            while(p1 != null || p2 != null || carry > 0)
+            {
+                int value = 0;
+                if (p1 != null)
+                {
+                    value += p1.val;
+                    p1 = p1.next;
+                }
+
+                if(p2 != null){
+                    value += p2.val;
+                    p2 = p2.next;
+                }
+
+                value += carry;
+
+                carry = value / 10;
+                value = value % 10;
+
+                ListNode temp = new ListNode(value);
+
+                current.next = temp;
+                current = temp;
+                
             }
-            if (l2==null && l1!=null){
-                var valAndrem2=FindValue(l1.val, 0, valAndrem.remainder);
-                l3.next=new ListNode(valAndrem2.valu);
-                l3=l3.next; l1=l1.next;
-                valAndrem=valAndrem2;
-            }
-            else if (l1!=null && l2!=null){
-            
-                var valAndrem2=FindValue(l1.val, l2.val, valAndrem.remainder);
-                l3.next=new ListNode(valAndrem2.valu);
-                l3=l3.next; l1=l1.next; l2=l2.next;
-                valAndrem=valAndrem2;
-            }
+
+            return preHead.next;
         }
-        if (valAndrem.remainder) l3.next=new ListNode(1);
-        return head;
-    }
-    public static (int valu, bool remainder) FindValue(int l1, int l2, bool remainder){
-        int valu=l1+l2+Convert.ToInt16(remainder);
-        if (valu>9){
-            remainder=true; valu-=10;
-        }
-        else remainder=false;
-        var valAndrem=(valu: valu, remainder:remainder);
-        return valAndrem;
-    }
 }
