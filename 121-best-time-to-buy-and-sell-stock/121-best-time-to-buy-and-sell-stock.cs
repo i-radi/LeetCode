d4cc2764bd98fdce6f7c41d0831417defa26d2c1
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-20 08:58:37 +0200

    Time: 427 ms (20.50%), Space: 46.6 MB (79.61%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public int MaxProfit (int[] prices) {
         if (prices == null || prices.Length <= 1) return 0;
-        var currentMin = int.MaxValue;
-        var maxProfit = int.MinValue;
-
-        for (int i = 0; i < prices.Length; i++) {
-            currentMin = Math.Min (prices[i], currentMin);
-            maxProfit = Math.Max (prices[i] - currentMin, maxProfit);
+        int maxProfit = 0;
+        int currentMin = prices[0];
+        for (int i = 1; i < prices.Length; i++) {
+            maxProfit = Math.Max (maxProfit, prices[i] - currentMin);
+            currentMin = Math.Min (currentMin, prices[i]);
         }
 
         return maxProfit;
