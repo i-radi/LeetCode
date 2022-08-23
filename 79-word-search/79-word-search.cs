7d632b46cf0e80418f8613c3fdb001d12b761722
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-23 09:10:24 +0200

    Time: 383 ms (61.40%), Space: 39.5 MB (79.93%) - LeetHub

---
+++ 79-word-search/79-word-search.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool Exist(char[][] board, string word) {
+        if(board == null) return false;
+        for(int i=0;i<board.Length;i++){
+            for(int j=0;j<board[i].Length;j++){
+                if(Search(board,word,i,j,0)) return true;        
+            }
+        }
+        return false;
+    }
+    
+    private bool Search(char[][] board,string word, int x,int y, int p){
+        if(x<0 || y<0 || x==board.Length || y==board[x].Length || board[x][y] != word[p]) return false;
+        if(p == word.Length-1) return true;
+        char original = board[x][y];
+        board[x][y] = '#';
+        var result =   Search(board, word, x-1,y,p+1) ||
+                       Search(board, word, x+1,y,p+1) ||
+                       Search(board, word, x,y-1,p+1) ||
+                       Search(board, word, x,y+1,p+1);
+        board[x][y] = original;
+        return result;
+        }
+}
