7bd1497550a2b3464038ed17b70c7fe4fb0cf494
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-08-29 09:45:39 +0200

    Time: 96 ms (62.70%), Space: 36.3 MB (92.97%) - LeetHub

--- 796-rotate-string/796-rotate-string.cs
+++ 796-rotate-string/796-rotate-string.cs
@@ -1,6 +1,5 @@
 public class Solution {
-    public bool RotateString(string s, string goal) {
-        string s1=s+s;
-        return s.Length==goal.Length && s1.Contains(goal);   
-    }
+    public bool RotateString(string s, string goal) 
+        => s.Length==goal.Length && $"{s}{s}".Contains(goal);   
+    
 }
