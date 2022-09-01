9cdeba5ef482bf5ddc1c556e8f5a6b345c4ea29b
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-01 19:34:50 +0200

    Time: 1456 ms (31.73%), Space: 42.7 MB (42.17%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.cs
+++ 482-license-key-formatting/482-license-key-formatting.cs
@@ -2,26 +2,14 @@ public class Solution
 {
     public string LicenseKeyFormatting(string s, int k) 
     {
-        string str = s.Replace("-", "").ToUpper();
-        int chunkSize = k;
-        int stringLength = str.Length;
-
-        string output = string.Empty;
-        for (int i = stringLength; i >= 0; i -= chunkSize)
+        s = s.Replace("-", "").ToUpper();
+        
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i=s.Length-k;i>0;i-=k)
         {
-            if (i < chunkSize)
-            {
-                if (i != 0)
-                {
-                    output = str.Substring(0, i) + (output == string.Empty ? "" : "-" + output);
-                }
-            }
-            else
-            {
-                output = str.Substring(i - chunkSize, chunkSize) + (output == string.Empty ? "" : "-" + output);
-            }
+            sb.Insert(i,"-");
         }
-
-        return output;
+        return sb.ToString();
     }
 }
