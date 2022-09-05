e4d3cfdb4c4b102d014bfc794bf6f1126e186284
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-05 08:12:13 +0200

    Time: 1859 ms (26.37%), Space: 42.4 MB (94.20%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cs
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cs
@@ -1,21 +1,19 @@
 public class Solution {
     public int SubarraySum(int[] nums, int k) {
-        if(nums == null || nums.Length == 0) return 0;
         var ans = 0;
-        var dict = new Dictionary<int,int>();
-        dict.Add(0,1);
-        var sum = 0;
-        foreach (var v in nums)
-        {
-            sum += v;
-            if(dict.ContainsKey(sum-k))
-            {
-                ans += dict[sum-k];
-            }
-
-            if(!dict.ContainsKey(sum)) dict.Add(sum,0);
-            dict[sum] += 1;
+        var presum = new int[nums.Length+1];
+        
+        for(int i=0;i<nums.Length;i++){
+            presum[i+1] = presum[i] + nums[i];
         }
+        
+        for(int start = 0;start < nums.Length; start++)
+        {
+            for(int end=start+1;end <= nums.Length;end++){
+                if((presum[end] - presum[start]) == k) ans++;
+            }
+        }
+        
         return ans;
     }
 }
