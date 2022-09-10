aca9ba2d38745bf5eff57f1dd722b1724d465baf
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-10 13:10:57 +0200

    Time: 44 ms (42.36%), Space: 25.2 MB (64.24%) - LeetHub

--- 69-sqrtx/69-sqrtx.cs
+++ 69-sqrtx/69-sqrtx.cs
@@ -1,13 +1,18 @@
-public class Solution
-{
-    public int MySqrt(int x)
-    {
-        if (x <= 1) return x;
-        for(long i = 0; i <= x; i++)
-        {
-            if (i * i > x) return (int)i - 1;
+public class Solution {
+    //Binary Search
+    public int MySqrt(int x) {
+        if(x<=1) return x;
+        int l = 1;
+        int r = x;
+        while(l<=r){
+            var m = l + (r-l)/2;
+            if(m > x/m){
+                r = m-1;
+            }else{
+                l = m+1;
+            }
         }
-        
-        return -1;       
+
+        return l - 1;
     }
 }
