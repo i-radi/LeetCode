41458e6650e1b12cfe8770faebd5f60d6fcb1e0f
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-16 06:20:56 +0200

    Time: 94 ms (92.65%), Space: 37.4 MB (58.78%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
@@ -1,24 +1,31 @@
 public class Solution {
-    public string LongestPalindrome(string s) {
-        var n = s.Length;
-        var dp = new bool[n,n];
-        var ans = 0;
-        var start = 0;
-        
-        for(var len = 1;len<=n;len++)
-            for(var l = 0;l<=n-len;l++)
-            {
-                var r = l + len - 1;
-                if(s[l] == s[r] && (r-l <= 2 || dp[l+1,r-1]))
-                {
-                    dp[l,r] = true;
-                    if(len > ans)
-                    {
-                        ans = len;
-                        start = l;
-                    }
-                }
-            }
-        return s.Substring(start, ans);
-    }
+   private int lo;
+   private int max;
+
+   public string LongestPalindrome(string s) {
+       if(string.IsNullOrEmpty(s)) return "";
+       if(s.Length == 1) return s;
+
+       for(int i=0;i<s.Length-1;i++){
+           LengthOfPalindrome(s, i, i);
+           LengthOfPalindrome(s, i, i+1);           
+       }
+
+       return s.Substring(lo,max);
+   }
+
+   private void LengthOfPalindrome(string s, int i, int j)
+   {
+       while(i>=0 && j<s.Length && s[i] == s[j])
+       {           
+           i--;
+           j++;           
+       }
+
+       if(j-i-1>max)
+       {
+           max = j-i-1;
+           lo = i+1;
+       }
+   }
 }
