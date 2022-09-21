7ef99531869cba589dd4eab08d287bd6a28f512d
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-21 20:23:50 +0200

    Time: 304 ms (5.22%), Space: 41.2 MB (62.42%) - LeetHub

--- 48-rotate-image/48-rotate-image.cs
+++ 48-rotate-image/48-rotate-image.cs
@@ -1,14 +1,27 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
-
+        transpose(matrix);
+        reverse(matrix);
+    }
+    
+    private void transpose(int[][] matrix) {
         int n = matrix.Length;
-        for (int i = 0; i < (n + 1) / 2; i ++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+    
+    private void reverse(int[][] matrix) {
+        int n = matrix.Length;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n/2;j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = tmp;
             }
         }
     }
