9b78b592624ac072ddf0c2ced8d466b9e72c08cd
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-23 06:43:59 +0200

    Time: 21 ms (96.58%), Space: 26.7 MB (16.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cs
+++ 62-unique-paths/62-unique-paths.cs
@@ -1,15 +1,16 @@
 public class Solution {
-   private Dictionary<string,int> dict = new Dictionary<string,int>();
    public int UniquePaths(int m, int n) {
-       if(m<1 || n<1) return 0;
-       if(m==1 || n==1) return 1;
-       var key = string.Format("{0}_{1}",m,n);
-       if(dict.ContainsKey(key)){
-           return dict[key];
-       }else{
-           var ret = UniquePaths(m-1,n) + UniquePaths(m,n-1);
-           dict.Add(key,ret);
-           return ret;
+       var DP = new int[m,n];
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(i==0 || j==0){
+                   DP[i,j] = 1;
+               }
+               else{
+                   DP[i,j] = DP[i-1,j] + DP[i,j-1];
+               }       
+           }
        }
+       return DP[m-1,n-1];
    }
 }
