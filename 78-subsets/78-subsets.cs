959bcea48445a40874e9fbbf74f3b3dcc49fe341
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-25 08:39:36 +0200

    Time: 227 ms (41.62%), Space: 40.8 MB (98.56%) - LeetHub

--- 78-subsets/78-subsets.cs
+++ 78-subsets/78-subsets.cs
@@ -1,19 +1,26 @@
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
         var ans = new List<IList<int>>();
+        if(nums == null || nums.Length == 0) return ans;
         
-        for(int i=0;i< (1 << nums.Length);i++)
-        {
-            var list = new List<int>();
-    
-            for(int j=0;j<nums.Length;j++)
-            {
-                if((i & (1 << j)) > 0) list.Add(nums[j]);
-            }
-            
-            ans.Add(list);
+        for(int c=0;c<=nums.Length;c++){
+            DFS(nums, 0, c, new List<int>(), ans);
         }
         
         return ans;
+    }
+    
+    private void DFS(int[] nums, int s, int c, IList<int> curr, IList<IList<int>> ans)
+    {
+        if(curr.Count == c){
+            ans.Add(new List<int>(curr));
+            return;
+        }
+        
+        for(int i=s;i<nums.Length;i++){
+            curr.Add(nums[i]);
+            DFS(nums, i+1, c, curr, ans);
+            curr.RemoveAt(curr.Count - 1);
+        }
     }
 }
