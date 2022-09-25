d19c48f68b459359409dc0466f31aea0b622833a
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-25 08:37:02 +0200

    Time: 158 ms (82.13%), Space: 42.6 MB (14.54%) - LeetHub

--- 78-subsets/78-subsets.cs
+++ 78-subsets/78-subsets.cs
@@ -1,20 +1,19 @@
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
         var ans = new List<IList<int>>();
-        if(nums == null || nums.Length == 0) return ans;
         
-        for(int i=0;i<(1<<nums.Length);i++){
-            var subSet = new List<int>();
-            
-            for(int b=0;b<nums.Length;b++){
-                if(((i >> b) & 1) == 1){
-                    subSet.Add(nums[b]);
-                }
+        for(int i=0;i< (1 << nums.Length);i++)
+        {
+            var list = new List<int>();
+    
+            for(int j=0;j<nums.Length;j++)
+            {
+                if((i & (1 << j)) > 0) list.Add(nums[j]);
             }
             
-            ans.Add(subSet);
+            ans.Add(list);
         }
-
+        
         return ans;
     }
 }
