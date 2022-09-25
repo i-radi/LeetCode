e9b59d227f54abe7028d0bc7b9cea17a2197e71f
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-25 08:40:11 +0200

    Time: 223 ms (45.17%), Space: 41.6 MB (27.08%) - LeetHub

--- 78-subsets/78-subsets.cs
+++ 78-subsets/78-subsets.cs
@@ -3,23 +3,17 @@ public class Solution {
         var ans = new List<IList<int>>();
         if(nums == null || nums.Length == 0) return ans;
         
-        for(int c=0;c<=nums.Length;c++){
-            DFS(nums, 0, c, new List<int>(), ans);
-        }
-        
+        DFS(nums, 0, new List<int>(), ans);
         return ans;
     }
     
-    private void DFS(int[] nums, int s, int c, IList<int> curr, IList<IList<int>> ans)
+    private void DFS(int[] nums, int s, List<int> curr, IList<IList<int>> ans)
     {
-        if(curr.Count == c){
-            ans.Add(new List<int>(curr));
-            return;
-        }
-        
-        for(int i=s;i<nums.Length;i++){
+        ans.Add(new List<int>(curr));
+        for(int i=s;i<nums.Length;i++)
+        {
             curr.Add(nums[i]);
-            DFS(nums, i+1, c, curr, ans);
+            DFS(nums, i+1, curr, ans);
             curr.RemoveAt(curr.Count - 1);
         }
     }
