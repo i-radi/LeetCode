99779f1edb5924ebd19cb8aa8eec6c6ba9054092
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-29 08:52:24 +0200

    Time: 415 ms (11.62%), Space: 48.6 MB (22.94%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cs
+++ 130-surrounded-regions/130-surrounded-regions.cs
@@ -1,33 +1,35 @@
 public class Solution {
     public void Solve(char[][] board) {
-        if(board == null || board.Length == 0) return ;
-        var m = board.Length;
-        var n = board[0].Length;
-
-        for(int i=0;i<m;i++){
-            DFS(board, m, n, i, 0); 
-            DFS(board, m, n, i, n-1);
-        }
-
-        for(int i=0;i<n;i++){
-            DFS(board, m, n, 0, i);
-            DFS(board, m, n, m-1, i);
-        }
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                if(board[i][j] == 'K') board[i][j] = 'O';
+        for(int i=0;i<board.Length;i++)
+            for(int j=0;j<board[i].Length;j++)
+            {
+                if(i == 0 || j == 0 || i == board.Length-1 || j == board[i].Length-1)
+                {
+                    BFS(board, i, j , 'O', 'K');
+                }
             }
-        }
+        
+        for(int i=0;i<board.Length;i++)
+            for(int j=0;j<board[i].Length;j++)
+            {
+                BFS(board, i, j , 'O', 'X');
+            }
+        
+        
+        for(int i=0;i<board.Length;i++)
+            for(int j=0;j<board[i].Length;j++)
+            {
+                BFS(board, i, j , 'K', 'O');
+            }
     }
-
-    private void DFS(char[][] board,int m, int n, int x, int y){
-        if(x < 0 || y < 0 || x >= m || y >= n || board[x][y] != 'O') return;
-        board[x][y] = 'K';
-        DFS(board, m, n, x-1, y);
-        DFS(board, m, n, x, y-1);
-        DFS(board, m, n, x+1, y);
-        DFS(board, m, n, x, y+1);
+    
+    private void BFS(char[][] board, int x, int y, char source, char changeTo)
+    {
+        if(x < 0 || y < 0 || x >= board.Length || y >= board[x].Length || board[x][y] != source) return;
+        board[x][y] = changeTo;
+        BFS(board, x-1, y, source, changeTo);
+        BFS(board, x+1, y, source, changeTo);
+        BFS(board, x, y-1, source, changeTo);
+        BFS(board, x, y+1, source, changeTo);
     }
 }
