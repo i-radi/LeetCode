0d21a0ccaff92635abfda1a90f782b688e79ff91
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-09-30 09:26:27 +0200

    Time: 910 ms (37.35%), Space: 63.7 MB (52.70%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cs
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cs
@@ -1,38 +1,35 @@
 public class Solution {
     public IList<IList<string>> Partition(string s) {
+        var dp = new bool[s.Length, s.Length];
         var ans = new List<IList<string>>();
-        if(string.IsNullOrEmpty(s)) return ans;
-        
-        DFS(s, 0, new List<string>(), ans);
+        DFS(s, 0, new List<string>(), ans, dp);
         return ans;
         
     }
     
-    private void DFS(string s, int index, IList<string> curr, IList<IList<string>> ans)
+    private void DFS(string s, int index, List<string> list, List<IList<string>> ans, bool[,] dp)
     {
-        if(s.Length == index)
+        if(index == s.Length)
         {
-            ans.Add(new List<string>(curr));
+            ans.Add(new List<string>(list));
             return;
         }
         
-        for(int i = index;i<s.Length;i++)
+        for(int i=index;i<s.Length;i++)
         {
-            if(IsPalindrome(s, index, i)){
-                curr.Add(s.Substring(index, i - index + 1));
-                DFS(s, i+1, curr, ans);
-                curr.RemoveAt(curr.Count - 1);
+            if(IsPalindrome(s, index, i, dp))
+            {
+                dp[index,i] = true;
+                list.Add(s.Substring(index, i - index + 1));
+                DFS(s, i+1, list, ans, dp);
+                list.RemoveAt(list.Count - 1);
             }
         }
     }
     
-    private bool IsPalindrome(string str, int s, int e)
+    private bool IsPalindrome(string s, int l, int r, bool[,] dp)
     {
-        while(s < e)
-        {
-            if(str[s++] != str[e--]) return false;
-        }
-        
-        return true;
+        if(s[l] == s[r] && (r-l <=2 || dp[l+1,r-1])) return true;
+        return false;
     }
 }
