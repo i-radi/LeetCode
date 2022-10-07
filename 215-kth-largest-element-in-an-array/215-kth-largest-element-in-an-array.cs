3613f3c6a6968597001fc0c3f9e846afda7057ee
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-07 09:08:11 +0200

    Time: 383 ms (29.89%), Space: 46.8 MB (33.24%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
@@ -1,35 +1,35 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        return Helper(nums, 0, nums.Length - 1, k);
+        return FindKthLargest(nums, 0, nums.Length-1, nums.Length - k);
     }
-
-    private int Helper(int[] nums, int low, int high, int k) {
-        if (low >= high)
-            return nums[low];
-
-        var pivot = nums[low];
-        var pivotIndex = low;
-        for (int i = low + 1; i <= high; i++) {
-            if (nums[i] <= pivot) {
-                Swap(nums, ++low, i);
+    
+    private int FindKthLargest(int[] nums, int start, int end, int k)
+    {
+        if (start > end) return int.MaxValue;
+        
+        var pivot = nums[end];
+        var left = start;
+        for(int i=start;i<end;i++)
+        {
+            if(nums[i] <= pivot){
+                Swap(nums, left++, i);
             }
         }
-
-        Swap(nums, pivotIndex, low);
-
-        var rightcount = high - low + 1;
-        if (rightcount == k)
-            return pivot;
-        else if (k < rightcount) {
-            return Helper(nums, low + 1, high, k);
-        } else {
-            return Helper(nums, pivotIndex, low - 1, k - rightcount);
-        }
+        
+        Swap(nums, left, end);
+        
+        if(left == k) 
+            return nums[left];
+        else if(left < k) //check right part
+            return FindKthLargest(nums, left+1, end, k);
+        else
+            return FindKthLargest(nums, start, left-1, k);
     }
-
-    private void Swap(int[] nums, int i1, int i2) {
-        var tmp = nums[i1];
-        nums[i1] = nums[i2];
-        nums[i2] = tmp;
+    
+    private void Swap(int[] nums, int A, int B)
+    {
+        var tmp = nums[A];
+        nums[A] = nums[B];
+        nums[B] = tmp;
     }
 }
