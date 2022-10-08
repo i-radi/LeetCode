c3baaf560ca35e5209d75f3bdb4d73e74be7da26
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-08 08:59:44 +0200

    Time: 160 ms (56.47%), Space: 40.7 MB (95.40%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+        if(left !=null && right !=null) return root;
+        
+        return left != null ? left : right;
+    }
+}
