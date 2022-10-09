801a371573701c1f4bea1b8c48b96ca53eae5d93
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-09 17:29:18 +0200

    Time: 315 ms (25.37%), Space: 49.1 MB (26.38%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cs
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cs
@@ -1,18 +1,20 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
         var n = nums.Length;
+        var l = new int[n];
+        var r = new int[n];
+        Array.Fill(l, 1);
+        Array.Fill(r, 1);
+        
+        for(int i=1;i<n;i++)
+            l[i] = l[i-1] * nums[i-1];
+        
+        for(int i=n-2;i>=0;i--)
+            r[i] = r[i+1] * nums[i+1];
+        
         var ans = new int[n];
-        Array.Fill(ans, 1);
-        
-        int l = 1, r = 1;
-        
         for(int i=0;i<n;i++)
-        {
-            ans[i] *= l;
-            ans[n - i - 1] *= r;
-            l *= nums[i];
-            r *= nums[n - i - 1];
-        }
+            ans[i] = l[i] * r[i];
         
         return ans;
     }
