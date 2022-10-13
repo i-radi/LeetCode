f1c38e220fda8dea376c7e5403a73a2fb83ae306
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-13 11:43:15 +0200

    Time: 278 ms (17.60%), Space: 37.9 MB (74.93%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
@@ -1,32 +1,19 @@
 public class Solution {
     public int LengthOfLIS(int[] nums) {
-        if(nums.Length == 1) return 1;
-        var dp = new List<int>();
-        for(int i=0;i<nums.Length;i++)
+        var dp = Enumerable.Repeat(1, nums.Length).ToArray();
+        var ans = 0;
+        
+        for(int i=0;i<nums.Length;i++) 
         {
-            var pos = BinarySearch(dp,nums[i]);
-            if(pos == dp.Count)
-                dp.Add(nums[i]);
-            else
-                dp[pos] = nums[i];
+            for(int j=i-1;j>=0;j--)
+            {
+                if(nums[i] > nums[j])
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+            }
+            
+            ans = Math.Max(ans, dp[i]);
         }
         
-        return dp.Count();
-    }
-    
-    private int BinarySearch(List<int> list, int target)
-    {
-        var l = 0;
-        var r = list.Count;
-        while(l < r)
-        {
-            var mid = (r-l)/2 + l;
-            if(list[mid] >= target)
-                r = mid;
-            else
-                l = mid+1;
-        }
-        
-        return l;
+        return ans;
     }
 }
