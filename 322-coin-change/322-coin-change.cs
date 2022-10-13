6c36b922844d655b0a02892a6fe9d309640676ce
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-13 11:47:37 +0200

    Time: 152 ms (70.20%), Space: 39.5 MB (70.20%) - LeetHub

--- 322-coin-change/322-coin-change.cs
+++ 322-coin-change/322-coin-change.cs
@@ -1,22 +1,16 @@
 public class Solution {
     public int CoinChange(int[] coins, int amount) {
-        if(amount < 1) return 0;
-        return CoinChange(coins, amount, new int[amount+1]);
-    }
-    
-    private int CoinChange(int[] coins, int amount, int[] memo)
-    {
-        if(amount < 0) return -1;
-        if(amount == 0) return 0;
-        if(memo[amount-1] != 0) return memo[amount-1];
-        int min = int.MaxValue;
-        foreach(var coin in coins) {
-            int result = CoinChange(coins, amount - coin, memo);
-            if(result >= 0 && result < min)
-                min = result + 1;
+        var dp = new int[amount+1];
+        Array.Fill(dp, amount+1);
+        dp[0] = 0;
+        for(int k=1;k<=amount;k++) {
+            for(int j=0;j<coins.Length;j++) {
+                if(k >= coins[j]) {
+                    dp[k] = Math.Min(dp[k], dp[k - coins[j]] + 1);
+                }
+            }
         }
         
-        memo[amount-1] = (min == int.MaxValue) ? -1 : min;
-        return memo[amount-1];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
