85090ac30b804a19dcc9744886ed5f5a5bb6c5c5
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-19 09:24:52 +0200

    Time: 101 ms (93.44%), Space: 42.8 MB (7.78%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cs
+++ 42-trapping-rain-water/42-trapping-rain-water.cs
@@ -1,24 +1,26 @@
 public class Solution {
     public int Trap(int[] height) {
-        var ans = 0;
-        var l = 0;
-        var r = height.Length - 1;
-        var l_max = height[l];
-        var r_max = height[r];
-        
-        while(l < r)
+        var n = height.Length;
+        var leftDP = new int[n];
+        var lMax = 0;
+        for(int i=0;i<n;i++)
         {
-            if(l_max <= r_max)
-            {
-                ans += l_max - height[l];
-                l_max = Math.Max(l_max, height[++l]);
-            }
-            else
-            {
-                ans += r_max - height[r];
-                r_max = Math.Max(r_max, height[--r]);
-            }
+            lMax = Math.Max(lMax, height[i]);
+            leftDP[i] = lMax - height[i];
         }
+        
+        var rMax = 0;
+        var rightDP = new int[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            rMax = Math.Max(rMax, height[i]);
+            rightDP[i] = rMax - height[i];
+        }
+        
+        var ans = 0;
+        
+        for(int i=0;i<n;i++)
+            ans += Math.Min(leftDP[i], rightDP[i]);
         
         return ans;
     }
