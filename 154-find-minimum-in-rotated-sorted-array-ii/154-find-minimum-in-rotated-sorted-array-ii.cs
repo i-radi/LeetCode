fdd6ffa176ec7065451c5017c543665ae3399bcc
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-21 05:50:34 +0200

    Time: 167 ms (15.34%), Space: 37.8 MB (67.20%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cs
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cs
@@ -1,13 +1,19 @@
 public class Solution {
     public int FindMin(int[] nums) {
-        return FindMin(nums,0, nums.Length-1);
-    }
-    
-    private int FindMin(int[] nums, int l , int r)
-    {
-        if(l+1 >= r) return Math.Min(nums[l],nums[r]);
-        if(nums[l] < nums[r]) return nums[l];
-        var mid = (r - l)/2 + l;
-        return Math.Min(FindMin(nums, l, mid), FindMin(nums, mid+1,r));
+        var l = 0;
+        var r = nums.Length - 1;
+        
+        while(l < r)
+        {
+            var mid = (r-l)/2 + l;
+            if(nums[mid] > nums[r])
+                l = mid + 1;
+            else if(nums[mid] < nums[r])
+                r = mid;
+            else // when num[mid] and num[r] are same
+                r--;
+        }
+        
+        return nums[l];
     }
 }
