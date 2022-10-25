1a93787db3f1876fc549235b1c8e097e23e3008e
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-10-25 08:35:48 +0200

    Time: 37 ms (80.00%), Space: 25.1 MB (90.00%) - LeetHub

---
+++ 479-largest-palindrome-product/479-largest-palindrome-product.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    
+    private static readonly int[] s_Answers = new [] {
+      0, 9, 987, 123, 597, 677, 1218, 877, 475
+    };
+    
+    public int LargestPalindrome(int n) => s_Answers[n];
+    
+}
