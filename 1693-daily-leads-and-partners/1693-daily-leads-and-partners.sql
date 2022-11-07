7c99df0c59d0de545d638b87295045499f94b244
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-11-07 07:33:45 +0200

    Time: 1883 ms (7.85%), Space: 0B (100.00%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM
+    DailySales
+GROUP BY
+    date_id,
+    make_name
