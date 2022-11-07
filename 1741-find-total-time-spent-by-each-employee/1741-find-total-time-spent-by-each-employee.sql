2c57803c86f0c18828d3d33e9bd33e06c34ff85d
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2022-11-07 07:34:57 +0200

    Time: 2937 ms (5.13%), Space: 0B (100.00%) - LeetHub

---
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT
+    event_day AS day,
+    emp_id,
+    SUM(out_time - in_time) AS total_time
+FROM
+    Employees
+GROUP BY
+    event_day,
+    emp_id
